2714334d94569031a2794bcc8118f5a2ffd1b0ab
Andressa <andressagss21@gmail.com>
2024-05-30 11:02:51 -0300

    search field integration

--- gallery/views.py
+++ gallery/views.py
@@ -8,3 +8,13 @@ def index(req):
 def image(req, photo_id): 
   photo = get_object_or_404(Photo, pk=photo_id)
   return render(req, 'gallery/image.html', {"photo": photo})
+
+def search(req):
+  photos = Photo.objects.order_by("date").filter(published=True)
+
+  if "search" in req.GET:
+    search_name = req.GET['search']
+    if search_name:
+      photos = photos.filter(name__icontains=search_name)
+
+  return render(req, 'gallery/search.html', {"cards": photos})
